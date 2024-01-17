e33b49384429cfd17c7278d4c8a4eba746f7473c
tsumme1 <55860177+tsumme1@users.noreply.github.com>
2024-01-17 13:07:40 -0500

    Add cosine similarity

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ Spike-based learning in Flax/JAX.
 Slax is a spiking neural network package, focusing on easy and efficient implementations of many training algorithms, especially online training. We will also branch into reinforcement learning.
 
 While the repository is private, install by cloning the repository, navigating to the local folder, and running `pip install .`
+
+## TO DO
+
+* Minor items
+  * Make OSTL and algs work with any number of carry states (only works with one now). This is necessary for general compatibility
+  * Add more neurons and surrogate derivatives (Currently only LIF and fast_sigmoid are fully operational)
+  * Make `offline_learning` compatible with batch normalization
+  * Determine best output for `gen_loss_landscape`
+  * Add more info to function comments, especially function outputs
+  * Code cleanup
+* Major items
+  * Add docs website
+  * Add package to PyPI
--- src/slax/utils.py
+++ src/slax/utils.py
@@ -186,4 +186,55 @@ class train_offline(nn.Module):
             return params,opt_state,s,loss,grad,state
         
         return loop(params,carry,batch,opt_state)
+    
+def layerwise_cosine_similarity(pytree_0,pytree_1):
+    '''
+    Computes the cosine similarity of each item between two pytrees with the same structure.
+
+    Args:
+        pytree_0: The first pytree with the same structure as pytree_1
+        pytree_1: The second pytree with the same structure as pytree_0
+    
+    Returns:
+        A pytree with the structure as the inputs. Each item contains the scalar cosine similarity value.
+    '''
+    return tree_map(lambda x,y: optax.cosine_similarity(x.flatten(),y.flatten),pytree_0,pytree_1)
+
+def global_cosine_similarity(pytree_0,pytree_1):
+    '''
+    Computes the cosine similarity of all elements between two pytrees. 
+
+    Args:
+        pytree_0: The first pytree
+        pytree_1: The second pytree
+    
+    Returns:
+        A pytree with the structure as the inputs. Each item contains the scalar cosine similarity value.
+    '''
+    return optax.cosine_similarity(ravel_pytree(pytree_0)[0],ravel_pytree(pytree_1)[0])
+
+def compare_grads(train_func,reference_params,reference_grad,train_func_args,comparison_func=layerwise_cosine_similarity):
+    '''
+    Performs a comparison function on a given reference pytree of gradients and a calculated pytree of gradients, using
+    a given training function and its arguments.
+
+    Args:
+        train_func: The returned function from calling `train_online_deffered` or a similar function with the same inputs
+        and outputs
+        reference_params: A pytree of the reference parameters
+        reference_grad: A pytree of the reference gradients
+        train_func_args: A tuple of the arguments for `train_func` (params,carry,batch,opt_state)
+        comparison_func: A function that takes in two pytrees and performs some comparison operation. Defaults to `layerwise_cosine_similarity'
+    
+    Returns:
+        The output of comparison_func
+    '''
+
+    params,carry,batch,opt_state = train_func_args
+    reference_params = tree_unflatten(tree_structure(params),tree_leaves(reference_params))
+    _,_,_,_,new_grad,_ = train_func(reference_params,carry,batch,opt_state)
+    reference_grad = tree_unflatten(tree_structure(new_grad),tree_leaves(reference_params))
+    return comparison_func(reference_grad,new_grad)
+    
+
 
